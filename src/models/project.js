c675a815cda9a7398df3dab201496bb704276f17
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-05-12 18:01:01 -0300

    Project model created

---
+++ src/models/project.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const ProjectSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	description: {
+		type: String
+	}
+})
