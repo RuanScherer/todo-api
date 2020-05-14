9e7cbde9a607d77442ed94cf9848e019ecdf82b0
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-05-14 17:42:56 -0300

    Connection changed to cluster

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 mongoose.set('useCreateIndex', true)
 
-mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://usuario_admin:xOdtYYXTOqTKeLhi@mycluster-lcfko.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true })
+//mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true, useUnifiedTopology: true })
 
 mongoose.Promise = global.Promise
 
